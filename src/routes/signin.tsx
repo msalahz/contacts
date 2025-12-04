b3f05b3304c1fd6cfea439d63d1a145ade636c6c
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-04 04:16:26 +0200

    feat: implement SigninForm in signin route for user authentication

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { SigninForm } from '@/features/users/components/signin-form'
 
 export const Route = createFileRoute('/signin')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/signin"!</div>
+  return (
+    <section className="flex flex-col items-center justify-center p-6 min-h-full">
+      <SigninForm />
+    </section>
+  )
 }
