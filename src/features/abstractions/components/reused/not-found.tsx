52c81db52f947c32e3629ef45c2d4ce1ec819587
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-04 19:01:37 +0200

    feat: enhance NotFound component with customizable message and navigation options

--- src/features/abstractions/components/reused/not-found.tsx
+++ src/features/abstractions/components/reused/not-found.tsx
@@ -1,13 +1,25 @@
 import { Link } from '@tanstack/react-router'
-import { Button } from '@/features/abstractions/components/primitives/button'
 
-export function NotFound() {
+export function NotFound({ children }: { children?: any }) {
   return (
-    <section className="relative overflow-hidden px-6 py-20 text-center">
-      <div>Not Found</div>
-      <Button asChild variant="link">
-        <Link to="/">Go to Home</Link>
-      </Button>
-    </section>
+    <div className="space-y-2 p-2">
+      <div className="text-gray-600 dark:text-gray-400">
+        {children || <p>The page you are looking for does not exist.</p>}
+      </div>
+      <p className="flex flex-wrap items-center gap-2">
+        <button
+          onClick={() => window.history.back()}
+          className="rounded-sm bg-emerald-500 px-2 py-1 text-sm font-black text-white uppercase"
+        >
+          Go back
+        </button>
+        <Link
+          to="/"
+          className="rounded-sm bg-cyan-600 px-2 py-1 text-sm font-black text-white uppercase"
+        >
+          Start Over
+        </Link>
+      </p>
+    </div>
   )
 }
