9859c1638060921f815b03ddbfe0cb5ee8c02352
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-06 01:45:03 +0200

    feat: remove unused server-only functions from utils

--- src/features/abstractions/lib/utils.ts
+++ src/features/abstractions/lib/utils.ts
@@ -3,17 +3,6 @@ import { twMerge } from 'tailwind-merge'
 
 import type { ClassValue } from 'clsx'
 
-import { senv } from '@/env'
-
 export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
-}
-
-// SERVER ONLY FUNCTION
-export function isDevServerOnly() {
-  return senv.ENV === 'development'
-}
-// SERVER ONLY FUNCTION
-export function isProdServerOnly() {
-  return senv.ENV === 'production'
 }
