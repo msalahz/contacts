a35b22558f01fc047e786f75a961c095a6535451
Mohammed Zaghloul <m.salahz86@gmail.com>
2025-12-07 01:42:50 +0200

    chore(eslint): update eslint configuration and dependencies
    - add @tanstack/eslint-plugin-query and @tanstack/eslint-plugin-router
    - include eslint-plugin-react-hooks and eslint-plugin-react-refresh
    - update eslint.config.js to incorporate new plugins and rules
    - add eslint-config-prettier to ensure consistent formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,25 @@
 //  @ts-check
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import pluginRouter from '@tanstack/eslint-plugin-router'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettierConfig from 'eslint-config-prettier'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  reactHooks.configs.flat['recommended-latest'],
+  reactRefresh.configs.recommended,
+  { ignores: ['*.config.js'] },
+  {
+    rules: {
+      'react/no-multi-comp': 'off',
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+    },
+  },
+  ...pluginQuery.configs['flat/recommended'],
+  ...pluginRouter.configs['flat/recommended'],
+  prettierConfig,
+]
